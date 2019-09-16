dfbf3fea8e94b8920d61559043f545e87ec2fbfc
Kazushi Kitaya <kazushikitaya@gmail.com>
2019-09-15 19:06:14 -0700

    add tests for RequestRewriter

--- pkg/scriproxy/rewriter_test.go
+++ pkg/scriproxy/rewriter_test.go
@@ -8,20 +8,14 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func newMockRequest(t *testing.T) *http.Request {
-	mockRequest, err := http.NewRequest(
-		"GET",
-		"http://example.com:3000/foo?bar=baz",
-		&bytes.Buffer{},
-	)
-
-	mockRequest.Header.Set("bar", "baz")
+func newRequest(t *testing.T, method, url string) *http.Request {
+	req, err := http.NewRequest(method, url, &bytes.Buffer{})
 
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	return mockRequest
+	return req
 }
 
 func testRequestRewriter(
@@ -45,10 +39,10 @@ func testRequestRewriter(
 }
 
 func TestRequestRewriter(t *testing.T) {
-	t.Run("RewriteHost", func(t *testing.T) {
+	t.Run("Host", func(t *testing.T) {
 		testRequestRewriter(
 			t,
-			newMockRequest(t),
+			newRequest(t, "GET", "http://example.com/"),
 			`req.host = "foo.com"`,
 			[]string{""},
 			func(t *testing.T, req *http.Request) {
@@ -57,59 +51,109 @@ func TestRequestRewriter(t *testing.T) {
 		)
 	})
 
-	t.Run("RewriteQuery", func(t *testing.T) {
-		testRequestRewriter(
-			t,
-			newMockRequest(t),
-			`req.url.query.set("qux", req.url.query.get("bar"))`,
-			[]string{""},
-			func(t *testing.T, req *http.Request) {
-				assert.Equal(t, "baz", req.URL.Query().Get("qux"))
+	t.Run("Query", func(t *testing.T) {
+		t.Run("GetAndSet",
+			func(t *testing.T) {
+				testRequestRewriter(
+					t,
+					newRequest(t, "GET", "http://example.com/?foo=bar"),
+					`req.url.query.set("baz", req.url.query.get("foo"))`,
+					[]string{""},
+					func(t *testing.T, req *http.Request) {
+						assert.Equal(t, "bar", req.URL.Query().Get("baz"))
+					},
+				)
+			},
+		)
+
+		t.Run("Del",
+			func(t *testing.T) {
+				testRequestRewriter(
+					t,
+					newRequest(t, "GET", "http://example.com/?foo=bar"),
+					`req.url.query.del("foo")`,
+					[]string{""},
+					func(t *testing.T, req *http.Request) {
+						assert.Equal(t, "", req.URL.Query().Get("foo"))
+					},
+				)
 			},
 		)
 	})
 
-	t.Run("RewriteHeader", func(t *testing.T) {
-		testRequestRewriter(
-			t,
-			newMockRequest(t),
-			`req.header.set("qux", req.header.get("bar"))`,
-			[]string{""},
-			func(t *testing.T, req *http.Request) {
-				assert.Equal(t, "baz", req.Header.Get("qux"))
-			},
-		)
+	t.Run("Header", func(t *testing.T) {
+		t.Run("GetAndSet", func(t *testing.T) {
+			req := newRequest(t, "GET", "http://example.com/")
+			req.Header.Set("foo", "bar")
+
+			testRequestRewriter(
+				t,
+				req,
+				`req.header.set("baz", req.header.get("foo"))`,
+				[]string{""},
+				func(t *testing.T, req *http.Request) {
+					assert.Equal(t, "bar", req.Header.Get("baz"))
+				},
+			)
+		})
+
+		t.Run("Del", func(t *testing.T) {
+			req := newRequest(t, "GET", "http://example.com/")
+			req.Header.Set("foo", "bar")
+
+			testRequestRewriter(
+				t,
+				req,
+				`req.header.del("foo")`,
+				[]string{""},
+				func(t *testing.T, req *http.Request) {
+					assert.Equal(t, "", req.Header.Get("foo"))
+				},
+			)
+		})
+
+		t.Run("Add", func(t *testing.T) {
+			testRequestRewriter(
+				t,
+				newRequest(t, "GET", "http://example.com/"),
+				`req.header.add("foo", "bar"); req.header.add("foo", "baz");`,
+				[]string{""},
+				func(t *testing.T, req *http.Request) {
+					assert.Equal(t, 2, len(req.Header["Foo"]))
+				},
+			)
+		})
 	})
 
-	t.Run("RewriteURL", func(t *testing.T) {
+	t.Run("URL", func(t *testing.T) {
 		testRequestRewriter(
 			t,
-			newMockRequest(t),
+			newRequest(t, "GET", "http://example.com/foo"),
 			`
 			req.url.scheme = req.url.scheme + "s"
-			req.url.host = "foo.com"
-			req.url.path = "bar"
+			req.url.host = req.url.host + ":3000"
+			req.url.path = req.url.path + "/bar"
 			`,
 			[]string{""},
 			func(t *testing.T, req *http.Request) {
 				assert.Equal(t, "https", req.URL.Scheme)
-				assert.Equal(t, "foo.com", req.URL.Host)
-				assert.Equal(t, "bar", req.URL.Path)
+				assert.Equal(t, "example.com:3000", req.URL.Host)
+				assert.Equal(t, "/foo/bar", req.URL.Path)
 			},
 		)
 	})
 
-	t.Run("UseLibrary", func(t *testing.T) {
+	t.Run("Library", func(t *testing.T) {
 		testRequestRewriter(
 			t,
-			newMockRequest(t),
+			newRequest(t, "GET", "http://example.com/"),
 			`
 			text := import("text")
-			req.url.path = text.join(["foo", "bar"], "/")
+			req.url.path = "/" + text.join(["foo", "bar"], "/")
 			`,
 			[]string{"text"},
 			func(t *testing.T, req *http.Request) {
-				assert.Equal(t, "foo/bar", req.URL.Path)
+				assert.Equal(t, "/foo/bar", req.URL.Path)
 			},
 		)
 	})
