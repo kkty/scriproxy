068cec928f22cb4355efe93ed61e45c956bfaf0d
Kazushi Kitaya <kazushikitaya@gmail.com>
2019-09-16 08:28:41 -0700

    add logging

--- go.mod
+++ go.mod
@@ -4,5 +4,10 @@ go 1.12
 
 require (
 	github.com/d5/tengo v1.24.3
+	github.com/pkg/errors v0.8.1 // indirect
+	github.com/rs/xid v1.2.1
 	github.com/stretchr/testify v1.4.0
+	go.uber.org/atomic v1.4.0 // indirect
+	go.uber.org/multierr v1.1.0 // indirect
+	go.uber.org/zap v1.10.0
 )
--- server.go
+++ server.go
@@ -9,8 +9,10 @@ import (
 	"net/http/httputil"
 	"os"
 	"strings"
+	"time"
 
 	"github.com/kkty/scriproxy/pkg/scriproxy"
+	"go.uber.org/zap"
 )
 
 func readBytesFromFile(fileName string) ([]byte, error) {
@@ -49,11 +51,60 @@ func main() {
 		log.Fatal(err)
 	}
 
+	logger, err := zap.NewProduction()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer logger.Sync()
+
+	times := make(map[*http.Request]time.Time)
+	loggers := make(map[*http.Request]*zap.Logger)
+
 	proxy := httputil.ReverseProxy{
 		Director: func(req *http.Request) {
-			if err := requestRewriter.Rewrite(req); err != nil {
-				log.Fatal(err)
-			}
+			logger := logger.With(
+				zap.String("method", req.Method),
+				zap.String("remote_addr", req.RemoteAddr),
+				zap.String("original_user_agent", req.UserAgent()),
+				zap.String("original_host", req.Host),
+				zap.String("original_url_scheme", req.URL.Scheme),
+				zap.String("original_url_host", req.URL.Host),
+				zap.String("original_url_path", req.URL.Path),
+				zap.String("original_url_query", req.URL.RawQuery),
+			)
+
+			requestRewriter.Rewrite(req)
+
+			logger = logger.With(
+				zap.String("host", req.Host),
+				zap.String("url_scheme", req.URL.Scheme),
+				zap.String("url_host", req.URL.Host),
+				zap.String("url_path", req.URL.Path),
+				zap.String("url_query", req.URL.RawQuery),
+			)
+
+			loggers[req] = logger
+			times[req] = time.Now()
+		},
+		ModifyResponse: func(res *http.Response) error {
+			loggers[res.Request].Info("received_response",
+				zap.Int("status_code", res.StatusCode),
+				zap.Duration("elapsed", time.Now().Sub(times[res.Request])))
+
+			delete(times, res.Request)
+			delete(loggers, res.Request)
+
+			return nil
+		},
+		ErrorHandler: func(w http.ResponseWriter, req *http.Request, err error) {
+			logger.Error("error_from_proxy",
+				zap.Duration("elapsed", time.Now().Sub(times[req])),
+				zap.Error(err))
+
+			delete(times, req)
+			delete(loggers, req)
 		},
 	}
 
